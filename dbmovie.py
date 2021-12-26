4c2b115d1cf6384a011db175a70b76972e7f8a31
somehow55 <somehow55@gmail.com>
2021-12-26 18:19:05 +0900

    211226 12/19 이후 공부

---
+++ dbmovie.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:1234@cluster0.8s2if.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbhandey
+
+# movie = db.movies.find_one({'title':'스파이더맨: 노 웨이 홈'});
+# star = movie['star']
+# print(star);
+#
+# all_movies = list(db.movies.find({'star':star}, {'_id':False}));
+# # all_movies = list(db.movies.find({조건문!}, {'_id':False}));
+#
+# for m in all_movies:
+#     print(m['title']);
+
+
+db.movies.update_one({'title':'스파이더맨: 노 웨이 홈'},{'$set':{'star':'0.00'}});
--- week3/lesson3-8.py
+++ week3/lesson3-8.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=cur&date=20211218', headers=headers)
